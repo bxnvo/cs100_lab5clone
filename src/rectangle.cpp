3b32aa588c042970b0ae47a54d35fd0f5438c30e
bxnvo <brandonvo27771@gmail.com>
2023-11-16 23:00:31 -0800

    added failing test branch

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -10,11 +10,13 @@ void Rectangle::set_height(int h) {
 }
 
 int Rectangle::area() {
-    return this->width * this->height;
+    //return this->width * this->height;
+    return 69;
 }
 
 int Rectangle::perimeter() {
-    return (2*this->width) + (2*this->height);
+    //return (2*this->width) + (2*this->height);
+    return 23;
 }
 
 Rectangle::Rectangle() : width(0), height(0){}
@@ -26,6 +28,6 @@ Rectangle::Rectangle(int w,int h) : width(w), height(h) {
     if (height < 0){
         height = height * -1;
     } */
-    assert(width >= 0 && "Width is negative");
-    assert(height >= 0 && "Height is negative");
+    //assert(width >= 0 && "Width is negative");
+    //assert(height >= 0 && "Height is negative");
 } 
